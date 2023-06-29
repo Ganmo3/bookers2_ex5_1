81db785ef97495da4001832915555dcb7cdb83a2
Ganmo3 <131627788+Ganmo3@users.noreply.github.com>
2023-06-29 08:31:54 +0000

    [add]routing

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,14 @@ class User < ApplicationRecord
   has_many :books, dependent: :destroy
   has_many :favorites, dependent: :destroy
   has_many :book_comments, dependent: :destroy
+  
+  # フォロー機能実装
+  has_many :active_relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :passive_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  
+  has_many :followings, through: :active_relationships, source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
+  
   has_one_attached :profile_image
 
   validates :name, presence: true, length: { minimum: 2, maximum: 20 }, uniqueness: true
@@ -14,6 +22,18 @@ class User < ApplicationRecord
 
   def get_profile_image
     (profile_image.attached?) ? profile_image : 'no_image.jpg'
+  end
+  
+  def follow(user)
+    active_relationships.create(followed_id: user.id)
+  end
+  
+  def unfollow(user)
+    active_relationships.find_by(followed_id: user.id).destroy
+  end
+  
+  def following?(user)
+    followings.include?(user)
   end
 
 end
